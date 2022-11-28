13fc7becbf58ed8e9ae191a91bb0ac79120b38a0
Dominic Minnich <dominicminnich@gmail.com>
2022-11-28 11:55:39 -0500

    prettier

--- src/DataBase.java
+++ src/DataBase.java
@@ -17,9 +17,8 @@ public class DataBase {
         BinaryTree<String> binaryTree = new BinaryTree<String>();
         for (int i = 0; i < employeesArray.size(); i++) {
             binaryTree.insert(employeesArray.retreiveAtIndex(i).getEmployeeID());
-            // print out the employeeID and first name
             System.out.println(employeesArray.retreiveAtIndex(i).getEmployeeID() + " "
-                    + employeesArray.retreiveAtIndex(i).getFirstName()+" "+i);
+                    + employeesArray.retreiveAtIndex(i).getFirstName() + " " + i);
         }
         return binaryTree;
     }
